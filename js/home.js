e376015ee0e4056ac5288a256aa9966bf40787cc
Khoirul Anam <anamskansamal@gmail.com>
2020-04-07 17:40:29 +0700

    update css, add bootsrap offline, test parallax server bootsrapnya lemmot tadi, jadi tak tambain file offline

---
+++ js/home.js
@@ -0,0 +1,12 @@
+// window.addEventListener("scroll", function() {
+//     var head1 = document.getElementById("head1");
+//     var pos = head1.getBoundingClientRect();
+
+//     if(pos.top >= 0 && pos.bottom <= window.innerHeight) {
+//         $("#nav").hide();
+//         alert("Hidden")
+//     }
+//     if(pos.top < window.innerHeight && pos.bottom >= 0) {
+//         $("#nav").show();
+//     }
+// });

763aa4203b0eaefd192c16f9245c1f7fa17ed400
Nafi'ul Anam <anamnafiul99@gmail.com>
2020-04-22 15:25:15 +0700

    reconstructing code layout

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
         <meta charset="UTF-8">
         <title>PT. Pindad (Persero) - Home</title>
         <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="css/main.css">
         <!--JQuery and Bootstrap lib-->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     </head>
@@ -119,7 +120,7 @@
             </div>
         </nav>
 
-        <!-- Include DIsini -->
+        <!-- Include Disini -->
         <?php
             include("page/".$_GET["page"].".php");
         ?>
@@ -196,7 +197,6 @@
                 </div>
             </div>
         </div>
-
         
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
--- page/berita.php
+++ page/berita.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="css/berita.css">
 <div class="full text-center">
     <span class="garis"></span>
     <h1 class="display-5 text-light"><dt><b>BERITA</b></dt></h1>

7da02032090b4573bb02cb350604d906f22e7650
Nafi'ul Anam <anamnafiul99@gmail.com>
2020-04-22 18:40:35 +0700

    add quicklink and desc

--- page/senjata.php
+++ page/senjata.php
@@ -12,3 +12,28 @@
         <li class="breadcrumb-item active" aria-current="page">Senjata</li>
     </ol>
 </nav>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="pb-5">
+                <h5 class="font-weight-bold">Produk & Jasa</h5>
+                <ul class="fa-ul list-aside my-3">
+                    <li><a href="index.php?page=senjata" class="active">Senjata</a></li>
+                    <li><a href="">Munisi</a></li>
+                    <li><a href="">Kendaraan Khusus</a></li>
+                    <li><a href="">Tempa Cor & Alat Perkeretaapian</a></li>
+                    <li><a href="">Alat Berat</a></li>
+                    <li><a href="">Bahan Peledak Komersial</a></li>
+                    <li><a href="">Cyber Security</a></li>
+                    <li><a href="">Anak Perusahaan</a></li>
+                    <li><a href="">Prosedur Pemesanan Produk Industrial</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <p>PT Pindad (Persero) sejak berdiri tahun 1983  telah memproduksi berbagai jenis senjata mulai dari senjata laras panjang, senjata genggam, pistol, dan lainnya. Setiap produksi diutamakan untuk menyuplai kebutuhan peralatan pertahanan dan keamanan nasional serta untuk memenuhi pemesanan dari pihak lain.</p>
+            <p>Produksi senjata terus ditingkatkan kualitasnya berdasarkan penelitian dan pengembangan dari tenaga-tenaga ahli Pindad bersama dengan pengguna produk untuk menetapkan spesifikasi yang dibutuhkan. Dalam setiap produksi, proses optimasi kami lakukan untuk memperoleh unjuk kerja dari senjata yang maksimal. Pemeriksaan dilakukan pada setiap proses manufaktur mulai dari penerimaan material sampai proses akhir pembuatan produk. Seluruh produk telah diuji dan memenuhi standar internasional salah satunya Mil STD. Sistem mutu selalu dipelihara dengan menerapkan sistem mutu ISO 9000-2008 yang disertifikasi oleh LRQA.</p>
+            <p>Senjata Pindad memiliki akurasi  yang baik dan ketahanan di medan peperangan sesuai dengan kebutuhan pertahanan dan keamanan. Beberapa senjata telah berhasil meraih prestasi lomba tembak antar angkatan darat se-Asia Tenggara (AARM) dan lomba tembak Angkatan Darat se-Asia Pasifik (ASAM), serta Lomba Tembak tahunan yang diselenggarakan oleh Angkatan Bersenjata Diraja Brunei (BISAM).</p>
+        </div>
+    </div>
+</div>

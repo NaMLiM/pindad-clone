bd1e790584744994edff7da92978042a52750468
Khoirul Anam <anamskansamal@gmail.com>
2020-04-23 14:25:07 +0700

    add gallery, katalog, majalah, video pages

---
+++ page/majalah.php
@@ -0,0 +1,90 @@
+<link rel="stylesheet" href="css/senjata.css">
+
+<div class="full text-center">
+    <span class="garis"></span>
+    <h1 class="display-5 text-light"><dt><b>MAJALAH</b></dt></h1>
+</div>
+
+<!---->
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item font-weight-bold"><a href="index.php">Pindad</a></li>
+        <li class="breadcrumb-item font-weight-bold"><a href="">Ruang Publik</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Majalah</li>
+    </ol>
+</nav>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="pb-5">
+                <h5 class="font-weight-bold text-dark">Ruang Publik</h5>
+                <ul class="fa-ul list-aside my-3">
+                    <li class="py-1"><a href="index.php?page=berita">Berita</a></li>
+                    <li class="py-1"><a href="index.php?page=siaran_pers">Siaran Pers</a></li>
+                    <li class="py-1"><a href="index.php?page=pindad_dalam_berita">Pindad Dalam Berita</a></li>
+                    <li class="py-1"><a href="index.php?page=majalah" class="active">Majalah</a></li>
+                    <li class="py-1"><a href="index.php?page=katalog_dan_brosur">Katalog Dan Brosur</a></li>
+                    <li class="py-1"><a href="index.php?page=gallery">Gallery</a></li>
+                    <li class="py-1"><a href="index.php?page=video">Video</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-9 text-dark">
+            <p>Informasi dan kegiatan dari PT Pindad (Persero) dalam 1 bulan Kami sajikan secara tematik dalam Pindad Update.</p>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <input type="text" class="search" name="search" placeholder="Quick Search">
+            <ul class="fa-ul list-aside my-3">
+                <li class="py-1"><a href="">All</a></li>
+                <li class="py-1"><a href="">Bulletin</a></li>
+                <li class="py-1"><a href="">Newsletter</a></li>
+            </ul>
+        </div>
+        <div class="col-md-9 row">
+            <div class="col-md-3 mb-4">
+                <div class="card">
+                    <img class="card-img-top" src="img/senjata/SPR_4.jpeg">
+                    <div class="card-body font-weight-bold">
+                        <p>SPR-4</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card">
+                    <img class="card-img-top" src="img/senjata/SPR_4.jpeg">
+                    <div class="card-body font-weight-bold">
+                        <p>SPR-4</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card">
+                    <img class="card-img-top" src="img/senjata/SPR_4.jpeg">
+                    <div class="card-body font-weight-bold">
+                        <p>SPR-4</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card">
+                    <img class="card-img-top" src="img/senjata/SPR_4.jpeg">
+                    <div class="card-body font-weight-bold">
+                        <p>SPR-4</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mb-4">
+                <div class="card">
+                    <img class="card-img-top" src="img/senjata/SPR_4.jpeg">
+                    <div class="card-body font-weight-bold">
+                        <p>SPR-4</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ page/video.php
@@ -0,0 +1,37 @@
+<link rel="stylesheet" href="css/senjata.css">
+
+<div class="full text-center">
+    <span class="garis"></span>
+    <h1 class="display-5 text-light"><dt><b>VIDEO</b></dt></h1>
+</div>
+
+<!---->
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item font-weight-bold"><a href="index.php">Pindad</a></li>
+        <li class="breadcrumb-item font-weight-bold"><a href="">Ruang Publik</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Video</li>
+    </ol>
+</nav>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="pb-5">
+                <h5 class="font-weight-bold text-dark">Ruang Publik</h5>
+                <ul class="fa-ul list-aside my-3">
+                    <li class="py-1"><a href="index.php?page=berita">Berita</a></li>
+                    <li class="py-1"><a href="index.php?page=siaran_pers">Siaran Pers</a></li>
+                    <li class="py-1"><a href="index.php?page=pindad_dalam_berita">Pindad Dalam Berita</a></li>
+                    <li class="py-1"><a href="index.php?page=majalah">Majalah</a></li>
+                    <li class="py-1"><a href="index.php?page=katalog_dan_brosur">Katalog Dan Brosur</a></li>
+                    <li class="py-1"><a href="index.php?page=gallery">Gallery</a></li>
+                    <li class="py-1"><a href="index.php?page=video" class="active">Video</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-9 text-dark">
+            <p>Video Profil Perusahaan PT. Pindad (Persero)</p>
+            <p>https://youtu.be/uFq-3O_nVGA</p>
+        </div>
+    </div>
+</div>

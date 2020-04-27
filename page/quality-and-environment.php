23be711472b0721784299e394214fb515e94e7f9
kanam129 <anamskansamal@gmail.com>
2020-04-27 15:05:45 +0700

    add some pages

---
+++ page/quality-and-environment.php
@@ -0,0 +1,67 @@
+<link rel="stylesheet" href="css/template-2.css">
+
+<div class="full text-center">
+    <span class="garis"></span>
+    <h1 class="display-5 text-light"><dt><b>SISTEM MANAJEMEN MUTU DAN KOMITMEN TERHADAP LINGKUNGAN</b></dt></h1>
+</div>
+
+<!---->
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item font-weight-bold"><a href="index.php">Pindad</a></li>
+        <li class="breadcrumb-item font-weight-bold"><a href="">Tentang Perusahaan</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Sistem Manajemen Mutu dan Komitmen Terhadap Lingkungan</li>
+    </ol>
+</nav>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="pb-5">
+                <h5 class="font-weight-bold text-dark">Tentang Perusahaan</h5>
+                <ul class="fa-ul list-aside my-3">
+                    <li class="py-1"><a href="index.php?page=vision-and-mission">Visi dan Misi</a></li>
+                    <li class="py-1"><a href="index.php?page=company-objective-and-target">Tujuan dan Sasaran Perusahaan</a></li>
+                    <li class="py-1"><a href="index.php?page=corporate-culture">Budaya Perusahaan</a></li>
+                    <li class="py-1"><a href="index.php?page=business-field-and-business-development">Bidang Usaha dan Perkembangan Usaha</a></li>
+                    <li class="py-1"><a href="index.php?page=legality">Legalitas</a></li>
+                    <li class="py-1"><a href="index.php?page=law-and-regulation">Peraturan dan Ketentuan Perusahaan</a></li>
+                    <li class="py-1"><a href="index.php?page=quality-and-environment" class="active">Sistem Manajemen Mutu dan Komitmen Terhadap Lingkungan</a></li>
+                    <li class="py-1"><a href="index.php?page=finance">Keuangan</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-9 text-dark isi">
+
+            <div class="isi-judul">Sistem Manajemen Mutu</div>
+
+            <p>Memiliki dedikasi yang tinggi untuk menghasilkan produk dan menyediakan jasa, yang  konsisten dalam hal mutu, pengiriman tepat waktu, harga kompetitif dan pelayanan terbaik.</p>
+
+            <p>Menerapkan dan mengembangkan Sistem Manajemen Mutu dan K3LH secara benar, tepat dan konsisten dengan komitmen mematuhi peraturan, perundangan dan persyaratan mutu & K3LH yang berlaku, baik dari pelanggan, pemerintah dan pihak terkait yang diikuti perusahaan.</p>
+
+            <p>Berupaya mencegah kecelakaan kerja, penyakit akibat kerja dan pencemaran lingkungan dengan menjamin setiap kegiatan/aktivitas perusahaan berwawasan lingkungan dan tidak menimbulkan dampak negatif terhadap lingkungan hidup.</p>
+
+            <p>Melakukan proses peningkatan yang berkelanjutan</p>
+
+            <p>Meningkatkan kepuasan kepada pelanggan.</p>
+
+            <div class="isi-judul">Komitmen Terhadap Lingkungan</div>
+
+            <p>Pengelolaan sampah dan limbah</p>
+
+            <p>Pelestarian lingkungan, seperti flora dan fauna</p>
+
+            <p>Kawasan dan lahan penelitian flora dan fauna</p>
+
+            <p>PT. Pindad (Persero) adalah Badan Usaha Milik Negara yang melaksanakan usaha terpadu dibidang peralatan pertahanan dan keamanan serta peralatan industrial yang mendukung pembangunan nasional.</p>
+
+            <p>Kebijakan ini dikomunikasikan kepada seluruh karyawan dan pihak terkait untuk dipahami dan diterapkan secara konsisten, serta ditinjau kesesuaian dan keefektifannya secara berkala.</p>
+
+            <p>PT. Pindad (Persero) menerapkan kebijakan untuk melaksanakan pembangunan, pengembangan perusahaan yang berwawasan lingkungan secara berkelanjutan dengan menciptakan lingkungan kerja yang aman, nyaman serta bebas dari kecelakaan, penyakit akibat kerja dan pencemaran lingkungan.</p>
+
+            <p>Unsur lingkungan yang dikelola meliputi :</p>
+
+            <p>Saat ini 60% dari lahan Pindad ditanami berbagai macam pepohonan dan lahan hijau, bahkan beberapa jenis pohon termasuk pohon langka. Dan yang lebih menarik, diantara lingkungan yang harmonis ini, hidup lebih dari 30 spesies burung. Beberapa burung termasuk burung yang langka hidup di perkotaan. Kondisi tersebut memungkinkan pecinta burung baik pelajar dan masyarakat umum melakukan penelitian kehidupan burung-burung tersebut.</p>
+
+        </div>
+    </div>
+</div>

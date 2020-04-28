afb6904089724c2d70dd986a86187328800bcd8a
Nafi'ul Anam <anamnafiul99@gmail.com>
2020-04-28 18:17:27 +0700

    add WYSIWYG HTML Editor

---
+++ admin/write-news.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://cdn.tiny.cloud/1/3oaj8v7pg3vxf0ezdpkje5mb6zhi9uyicn04nqok4vxmdazx/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+</head>
+<body>
+  <textarea>
+    Test
+  </textarea>
+  <script>
+    tinymce.init({
+      selector: 'textarea',
+      plugins: '',
+      toolbar: '',
+      toolbar_mode: 'floating',
+      });
+  </script>
+</body>
+</html>

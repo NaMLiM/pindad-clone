9453267340f36b052f91ecc157cb055f458dd1dc
Nafi'ul Anam <anamnafiul99@gmail.com>
2020-04-28 19:00:56 +0700

    update write-news layout

--- admin/write-news.php
+++ admin/write-news.php
@@ -1,19 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <script src="https://cdn.tiny.cloud/1/3oaj8v7pg3vxf0ezdpkje5mb6zhi9uyicn04nqok4vxmdazx/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <title>Write News</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/write-news.css">
 </head>
 <body>
-  <textarea>
-    Test
-  </textarea>
-  <script>
-    tinymce.init({
-      selector: 'textarea',
-      plugins: '',
-      toolbar: '',
-      toolbar_mode: 'floating',
-      });
-  </script>
+    <header>
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="dashboard.php">
+                <i class="fas fa-arrow-circle-left"></i>
+                Back
+            </a>
+            <input type="text" placeholder="Judul" name="filename" class="judul" size="100">
+            <input type="button" value="Submit" name="submit">
+        </nav>
+    </header>
+    <textarea class="editor">
+    </textarea>
+    <!-- JS library -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/7b59756246.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.19/jquery.touchSwipe.min.js"></script>
+    <script src="https://cdn.tiny.cloud/1/3oaj8v7pg3vxf0ezdpkje5mb6zhi9uyicn04nqok4vxmdazx/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>
+        tinymce.init({
+            selector: '.editor',
+            plugins: '',
+            toolbar: '',
+            toolbar_mode: 'floating',
+            });
+    </script>
 </body>
 </html>

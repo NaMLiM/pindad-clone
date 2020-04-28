88db8ea95434ae8d54154fcb32bdbcfdc70f90ff
Khoirul Anam <anamskansamal@gmail.com>
2020-04-28 09:19:15 +0700

    pages pada perusahaan > managemen selesai

---
+++ page/direksi.php
@@ -0,0 +1,87 @@
+<link rel="stylesheet" href="css/template-2.css">
+
+<div class="full text-center">
+    <span class="garis"></span>
+    <h1 class="display-5 text-light"><dt><b>DIREKSI</b></dt></h1>
+</div>
+
+<!---->
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item font-weight-bold"><a href="index.php">Pindad</a></li>
+        <li class="breadcrumb-item font-weight-bold"><a href="">Perusahaan</a></li>
+        <li class="breadcrumb-item font-weight-bold"><a href="">Managemen</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Direksi</li>
+    </ol>
+</nav>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="pb-5">
+                <h5 class="font-weight-bold text-dark">Tentang Perusahaan</h5>
+                <ul class="fa-ul list-aside my-3">
+                    <li class="py-1"><a href="index.php?page=organization-structure">Struktur Organisasi</a></li>
+                    <li class="py-1"><a href="index.php?page=dewan-komisaris">Dewan Komisaris</a></li>
+                    <li class="py-1"><a href="index.php?page=komite-audit">Komite Audit</a></li>
+                    <li class="py-1"><a href="index.php?page=direksi" class="active">Direksi</a></li>
+                    <li class="py-1"><a href="index.php?page=sesper-dan-kepala-spi">Sesper dan Kepala SPI</a></li>
+                    <li class="py-1"><a href="index.php?page=subsidiaries">Anak Perusahaan</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-9 text-dark isi">
+            <div class="isi-judul">DIREKTUR UTAMA</div>
+            <p>r. Abraham Mose, MM</p>
+            <div class="row mb-5">
+                <div class="col-md-3">
+                    <img src="img/direksi/Abraham_Mose_(web)11.jpg" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-9">Abraham Mose lahir di Gorontalo 55 tahun yang lalu. Beliau menyelesaikan pendidikan sarjana di jurusan Teknik Mesin Universitas Hasanuddin dan magister manajemen di Universitas Padjadjaran. Pada 1995, ia menjabat sebagai General Manager untuk Sistem Pengendalian di PT. Len Industri. Kemudian dari 2000-2002, Abraham Mose menjabat sebagai General Manager Signalling untuk bisnis transportasi kereta api. Pada 2002 hingga 2007, Abraham Mose menjadi General Manager Perhubungan dan pada 2007 menjadi Direktur Pemasaran. Sampai pada akhirnya di Agustus 2012, Abraham Mose dilantik menjadi Direktur Utama PT. Len Industri. Abraham Mose diangkat menjadi Direktur Utama PT Pindad (Persero) lewat Surat Keputusan Menteri BUMN nomor SK-169 / MBU / 08/2016.</div>
+            </div>
+
+            <div class="isi-judul mt-5">DIREKTUR BISNIS PRODUK PERTAHANAN & KEAMANAN</div>
+            <p>Heri Heriswan, SE, MBA</p>
+            <div class="row mb-3">
+                <div class="col-md-3">
+                    <img src="img/direksi/Heri_Dirbisnishankam_web11.jpg" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-9">Heri Heriswan lahir di Tasikmalaya. Heri mendapatkan gelar Sarjana Ekonomi (Manajemen) dari Universitas Proklamasi Yogyakarta dan Master of Business & Administration dari Institut Teknologi Bandung. Heri Heriswan lama berkiprah di PT. Dahana dan terakhir menjabat sebagai Direktur Teknologi & Pengembangan. 
+                <br><br>
+                Heri Heriswan diangkat sebagai Direktur Bisnis Produk Pertahanan & Keamanan berdasarkan surat Keputusan Menteri BUMN Nomor: SK-103/MBU/05/2019 tanggal 28 Mei 2019. Salinan Surat Keputusan Menteri diserahkan oleh Deputi Bidang Pertambangan, Industri Strategis, dan Media Kementerian BUMN, Fajar Harry Sampurno.</div>
+            </div>
+
+            <div class="isi-judul mt-5">DIREKTUR BISNIS PRODUK INDUSTRIAL</div>
+            <p>Heru Puryanto, Dipl, -Ing., MM/p>
+            <div class="row mb-3">
+                <div class="col-md-3">
+                    <img src="img/direksi/Heru_Puryanto_(Web)3.jpg" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-9">Heru Puryanto lahir di Turen, 9 April 1971. Setelah menyelesaikan program S-1 di Georg-Simon-Ohm Jerman, Heru melanjutkan studi program Magister di STT Telkom (Telkom University). Sebelum menjadi Direksi, Heru menjabat sebagai VP Teknologi & Pengembangan di  PT Pindad (Persero).
+                <br><br>
+                Pengangkatan Heru menjadi Direktur Bisnis Produk Industrial berdasarkan SK-29/MBU/01/2018 tanggal 22 Januari 2018. Salinan Surat Keputusan Menteri diserahkan  oleh Deputi Bidang Infrastruktur Bisnis Kementerian BUMN, Hambra.</div>
+            </div>
+
+            <div class="isi-judul mt-5">DIREKTUR KEUANGAN & ADMINISTRASI</div>
+            <p>Wildan Arief, SE, Ak, MBA, CPMA</p>
+            <div class="row mb-3">
+                <div class="col-md-3">
+                    <img src="img/direksi/Wildan_Arief_(web)3.jpg" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-9">Wildan Arief lahir di Malang, 4 Juni 1972. Wildan menyelesaikan program studi S1 di Universitas Brawijaya dan melanjutkan program Magister Administrasi Bisnis di Institut Teknologi Bandung. Sebelum menjadi anggota Direksi PT Pindad (Persero), Wildan berkarir di PT Krakatau Steel (Persero) Tbk dan menjabat sebagai General Manager Corporate Finance. 
+                <br><br>
+                Wildan Arief diangkat sebagai Direktur Keuangan & Administrasi PT Pindad (Persero) berdasarkan surat Keputusan Menteri BUMN Nomor: SK-50/MBU/02/2018 tanggal 21 Februari 2018. Salinan Surat Keputusan Menteri diserahkan oleh Deputi Bidang Pertambangan, Industri Strategis, dan Media Kementerian BUMN, Fajar Harry Sampurno.</div>
+            </div>
+
+            <div class="isi-judul mt-5">DIREKTUR TEKNOLOGI & PENGEMBANGAN</div>
+            <p>Dr. Ir. Ade Bagdja, MME</p>
+            <div class="row mb-3">
+                <div class="col-md-3">
+                    <img src="img/direksi/Ade_Bagdja_web2.jpg" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-9">Ade Bagdja menyelesaikan pendidikan sarjana di Institut Teknologi Bandung jurusan Teknik Mesin pada tahun 1990 dan melanjutkan program magister dan doktoral di jurusan Mechanical Production Engineering di Universitas Leuven Belgia pada tahun 1996 dan 2001. Sebelum menduduki jabatannya sekarang, Ade Bagdja pernah menduduki beberapa jabatan seperti Deputi Direktur Penelitian dan Pengembangan, Kepala Divisi Senjata, dan Direktur Sistem Senjata. 
+                <br><br>
+                Ade Bagdja diangkat sebagai Direktur Teknologi & Pengembangan PT Pindad (Persero) berdasarkan surat Keputusan Menteri BUMN Nomor: SK-144/MBU/05/2018. Salinan Surat Keputusan Menteri diserahkan oleh Deputi Bidang Pertambangan, Industri Strategis, dan Media Kementerian BUMN, Fajar Harry Sampurno.</div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ page/subsidiaries.php
@@ -0,0 +1,166 @@
+<link rel="stylesheet" href="css/template-2.css">
+
+<div class="full text-center">
+    <span class="garis"></span>
+    <h1 class="display-5 text-light"><dt><b>ANAK PERUSAHAAN</b></dt></h1>
+</div>
+
+<!---->
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item font-weight-bold"><a href="index.php">Pindad</a></li>
+        <li class="breadcrumb-item font-weight-bold"><a href="">Perusahaan</a></li>
+        <li class="breadcrumb-item font-weight-bold"><a href="">Managemen</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Anak Perusahaan</li>
+    </ol>
+</nav>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="pb-5">
+                <h5 class="font-weight-bold text-dark">Tentang Perusahaan</h5>
+                <ul class="fa-ul list-aside my-3">
+                    <li class="py-1"><a href="index.php?page=organization-structure">Struktur Organisasi</a></li>
+                    <li class="py-1"><a href="index.php?page=dewan-komisaris">Dewan Komisaris</a></li>
+                    <li class="py-1"><a href="index.php?page=komite-audit">Komite Audit</a></li>
+                    <li class="py-1"><a href="index.php?page=direksi">Direksi</a></li>
+                    <li class="py-1"><a href="index.php?page=sesper-dan-kepala-spi">Sesper dan Kepala SPI</a></li>
+                    <li class="py-1"><a href="index.php?page=subsidiaries" class="active">Anak Perusahaan</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-9 text-dark isi">
+            <p class="bg-info text-white p-1" style="display: inline;">ANAK PERUSAHAAN</p><br>
+            <img src="img/subsidiaries/PEI-pindad.png" class="img-fluid w-50 my-5" alt="">
+            <div class="isi-judul">PT Pindad Enjiniring Indonesia (PT PEI)</div>
+            <p>PT Pindad Enjiniring Indonesia (PT PEI) semula bernama PT Daun Cakra Bhakti yang didirikan di Bandung pada tanggal 7 Oktober 1992 berdasarkan Akta Notaris Ny. Hj. Imas Tarwiah Soedrajat, SH., M.H.  Nomor 34 dan disahkan oleh Menteri Kehakiman Nomor C2-7444 HT.01.01 Tahun 1993 tanggal 23 Agustus 1993, kemudian diubah dengan Akta Berita Acara Rapat Umum Pemegang Saham (RUPS) “PT Daun Cakra Bhakti” Nomor 88 tanggal 31 Desember 1999 dari Notaris Ny. Hj. Imas Tarwiah Soedrajat, SH., M.H. bahwa nama perseroan diubah menjadi PT Cakra Mandiri Pratama Indonesia dan telah mendapatkan pengesahan dari Menteri Kehakiman dan HAM Nomor C-21829 HT.01.04 tahun 2002 tanggal 8 November 2002, dan terakhir diubah berdasarkan Keputusan Rapat Umum Pemegang Saham Luar Biasa (RUPSLB) yang dicatat dengan Akta Notaris Rita Evryani, SH. Nomor 2 tanggal 4 Desember 2015 menyatakan bahwa PT Cakra Mandiri Pratama Indonesia mengubah namanya menjadi PT Pindad Enjiniring Indonesia disingkat PT Pindad Enjiniring dan telah disahkan melalui Keputusan Menteri Hukum dan Hak Asasi Manusia Republik Indonesia Nomor AHU/0948590.AH.01.02 Tahun 2015. Perusahaan mulai menetapkan keputusan ini pada tanggal 1 Januari 2016 berdasarkan edaran No B/1/PE/BD/1/2016.</p>
+
+            <p>PT Pindad Enjiniring Indonesia (PT PEI) memiliki anak perusahaan yaitu PT Pindad Medika Utama (PT PMU), PT Pindad International Logistic (PT PIL) dan PT Pindad Global Sources & Trading (PT PGST).</p>
+
+            <p>Bidang usaha </p>
+
+            <p>Perdagangan, pemborongan, pengembang (developer), perindustrian, mekanikal elektrikal, pertanian, angkutan, jasa, percetakan dan pertambangan serta bidang pelayanan kesehatan (pelayanan medik, penunjang medik dan apotik).</p>
+
+            <p>Alamat</p>
+
+            <p>Jl. Jenderal Gatot Subroto No 517 Kel. Sukapura Kec. Kiaracondong Bandung - 40284</p>
+
+            <p>Instagram  : @Pindad_enjiniringid</p>
+
+            <p>Facebook  : @PT.Pindad Enjiniring Indonesia</p>
+
+            <p>Twiter  : @Pindadenjind</p>
+
+            <p>Email  : cakramails@gmail.com & Tim.Pindadenjiniring@gmail.com</p>
+
+            <p>Komisaris</p>
+
+            <p>Komisaris Utama  : Wildan Arief</p>
+
+            <p>Komisaris  : Prihadi Agus Irianto</p>
+
+            <p>Komisaris  : D. Suganda</p>
+
+            <p>Direksi</p>
+
+            <p>Direktur Utama  : Sena Maulana</p>
+
+            <p>Direktur Administrasi & Keuangan : Adferkol Mayoh</p>
+
+            <p>Direktur Operasional  : Agus Edy Suprihanto</p>
+
+            <br><br>
+            <p class="bg-info text-white p-1" style="display: inline;">CUCU PERUSAHAAN</p><br>
+            <img src="img/subsidiaries/LOGO_PMU_31.jpg" class="img-fluid w-50 my-5" alt="">
+            <div class="isi-judul">PT Pindad Medika Utama</div>
+            <p>Bidang Usaha </p>
+
+            <p>Bidang pelayanan kesehatan (Pelayanan Medik, Penunjang Medik dan Instalasi Farmasi), yaitu Rawat Jalan, Rawat Inap, Instalasi Gawat Darurat, Unit Hemodialisa, Kamar Operasi, Konsultasi Psikologi dan Gizi, Pelayanan Penunjang (Laboratorium, Radiologi, Rehabilitasi Medik, Instalasi Farmasi dan Instalasi Gizi). </p>
+
+            <p>Alamat</p>
+
+            <p>Jl. Jenderal Gatot Subroto No 517 Kel. Sukapura Kec. Kiaracondong Bandung – 40284 &</p>
+
+            <p>Jl. Panglima Sudirman No 229 Kec. Turen, Malang 65175</p>
+
+            <p>Email  : pemasaran@rspindad.com & rspindad0665@gmail.com</p>
+
+            <p>Website  : www.rspindad.com</p>
+
+            <p>Komisaris Utama  : Tuning Rudyati</p>
+
+            <p>Komisaris PT PMU  : Hengky Kaluara</p>
+
+            <p>Direktur Utama PMU  : Lia Yuliani</p>
+
+            <p>Direktur Pengembangan Bisnis & SDM : -</p>
+            
+            <img src="img/subsidiaries/PIL-Pindad.png" class="img-fluid w-50 my-5" alt="">
+            <div class="isi-judul">PT Pindad International Logistic (PT PIL)</div>
+            <p>Bidang Usaha</p>
+
+            <p>Bidang jasa, terutama jasa pergudangan, transportasi, kepabeanan baik ekspor maupun impor serta jasa pengurusan freight dan movers.</p>
+
+            <p>Alamat </p>
+
+            <p>Jl. Jenderal Gatot Subroto No 517 Kel. Sukapura Kec. Kiaracondong Bandung - 40284</p>
+
+            <img src="img/subsidiaries/PGST-Pindad.png" class="img-fluid w-50 my-5" alt="">
+            <div class="isi-judul">PT Pindad Global Sources & Trading (PT PGST)</div>
+            <p>PT PGST memiliki kemampuan dan pengetahuan yang berharga sebagai pemasok umum dan kontraktor untuk memberikan layanan perdagangan yang memuaskan sesuai dengan tantangan dan persyaratan pelanggan. PT PGST memiliki agen untuk Produk Pertahanan, Keamanan, Industri, Listrik, Telekomunikasi dan Kereta Api.</p>
+
+            <p>Bidang Usaha </p>
+
+            <p>Perdagangan, jasa dan industri </p>
+
+            <p>Alamat </p>
+
+            <p>Jl. Jenderal Gatot Subroto No 517 Kel. Sukapura Kec. Kiaracondong Bandung - 40284</p>
+
+            <p>Komisaris <br>
+            Komisaris : Tundjung Inderawan <br>
+            Komisaris : Edy Purwanto <br>
+            Komisaris : I Wayan Sutama <br>
+            Direksi <br>
+            Direktur Utama : Wahyu Sofiadi <br>
+            Direksi : Ade Ahmad Suradi <br>
+            Direksi : Didi Suryana</p>
+
+            <br><br>
+            <p class="bg-info text-white p-1" style="display: inline;">CUCU PERUSAHAAN</p><br>
+            <img src="img/subsidiaries/man.jpg" class="img-fluid w-50 my-5" alt="">
+            <div class="isi-judul">PT MAN Diesel & Turbo</div>
+            <p>Bidang Usaha </p>
+
+            <p>1.  Jasa pemasangan dan pemeliharaan mesin turbin</p>
+
+            <p>2.  Perdagangan impor</p>
+
+            <p>3.  Instalasi listrik</p>
+
+            <p>4.  Aktivitas keinsyuran dan konsultasi teknis.</p>
+
+            <p>Alamat </p>
+
+            <p>Jl. H.R. Rasuna Said, Block X2, Kav. 6 Jakarta 12950 Unit #17-01, 17th Floor Menara Palma, </p>
+
+            <p>Telepon : 62-021-5795 7490</p>
+
+            <p>Fax  : 62-021-5795 7491</p>
+
+            <img src="img/subsidiaries/Logo_IPMS.png" class="img-fluid w-50" alt="">
+            <div class="isi-judul">PT Inti Pindad Mitra Sejati</div>
+            <p>Bidang Usaha</p>
+
+            <p>1. Bidang Pembangunan : pemborong, konstruksi gedung, menara, jembatan, landasan udara, Civil Mechanical Eletrical (CME), Teknologi Informasi, Outside Plant (OSP), dan lain-lain.</p>
+
+            <p>2. Bidang Perindustrian : plastik dan sejenisnya, logam, manufaktur, dan fabrikasi. </p>
+
+            <p>3. Perdagangan Umum</p>
+
+            <p>Alamat </p>
+
+            <p>Gedung Pusat Teknologi Lt.3 PT INTI (Persero) Jl. Moh. Toha No. 77, Bandung - 40253</p>
+        </div>
+    </div>
+</div>

127ea3b869b050a49b86dbc95e01cc114e120205
Nafi'ul Anam <anamnafiul99@gmail.com>
2020-04-28 15:51:55 +0700

    add dashboard (wip) and fix

---
+++ admin/dashboard.php
@@ -0,0 +1,38 @@
+<?php
+    include "system/connection.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/dashboard.css">
+    <title>Dashboard - nama_admin</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2 sidebar">
+                <div class="user">
+                    <div class="user-image">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="user-info text-center">
+                        <p>nama_admin</p>
+                        <input type="button" value="Logout" class="logout-btn">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-10">
+                Test
+            </div>
+        </div>
+    </div>
+    <!-- Javascript Lib -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/7b59756246.js" crossorigin="anonymous"></script>
+</body>
+</html>

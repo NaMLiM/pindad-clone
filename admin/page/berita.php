d9fdf0c8876e2d2d1e67ad2041d955e1c8b167d7
kanam129 <anamskansamal@gmail.com>
2020-04-29 13:00:42 +0700

    search

--- admin/page/berita.php
+++ admin/page/berita.php
@@ -1,31 +1,33 @@
 <div class="mt-4 w-100">
-    <input type="search" placeholder="Search" name="search" class="search ml-auto float-right">
+    <input type="search" placeholder="Search" id="search" name="search" class="search ml-auto float-right">
 </div>
 
 <table class="table" id="tabel">
-    <th style="background-color: orange; text-align: center;">ID</th>
-    <th style="background-color: orange; text-align: center;">Judul Berita</th>
-    <th style="background-color: orange; text-align: center;">Tanggal Berita</th>
-    <th style="background-color: orange; text-align: center;">Isi Berita</th>
-    <th style="background-color: orange; text-align: center;">Gambar Berita</th>
-    <th style="background-color: orange; text-align: center;">Kategori Berita</th>
-    
-    <?php
-        $select = mysqli_query($connection, "select * from berita");
-        while($data = mysqli_fetch_array($select)){
-            echo "
-                <tr data-toggle='modal' data-target='#modelId'>
-                    <td class='text-center'>".$data["id_berita"]."</td>
-                    <td class='text-center'>".$data["judul_berita"]."</td>
-                    <td class='text-center'>".$data["tanggal"]."</td>
-                    <td class='text-center'>".$data["isi"]."</td>
-                    <td class='text-center'>".$data["gambar"]."</td>
-                    <td class='text-center'>".$data["kategori"]."</td>
-                </tr>
-            ";
-        }
-    ?>
-
+    <thead>
+        <th style="background-color: orange; text-align: center;">ID</th>
+        <th style="background-color: orange; text-align: center;">Judul Berita</th>
+        <th style="background-color: orange; text-align: center;">Tanggal Berita</th>
+        <th style="background-color: orange; text-align: center;">Isi Berita</th>
+        <th style="background-color: orange; text-align: center;">Gambar Berita</th>
+        <th style="background-color: orange; text-align: center;">Kategori Berita</th>
+    </thead>
+    <tbody id="tbody">
+        <?php
+            $select = mysqli_query($connection, "select * from berita");
+            while($data = mysqli_fetch_array($select)){
+                echo "
+                    <tr data-toggle='modal' data-target='#modelId'>
+                        <td class='text-center'>".$data["id_berita"]."</td>
+                        <td class='text-center'>".$data["judul_berita"]."</td>
+                        <td class='text-center'>".$data["tanggal"]."</td>
+                        <td class='text-center'>".$data["isi"]."</td>
+                        <td class='text-center'>".$data["gambar"]."</td>
+                        <td class='text-center'>".$data["kategori"]."</td>
+                    </tr>
+                ";
+            }
+        ?>
+    </tbody>
 </table>
 
 <!-- Modal -->

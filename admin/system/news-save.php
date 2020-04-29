85a14517f730a7d922a0fee355834be40a71c3ab
Nafi'ul Anam <anamnafiul99@gmail.com>
2020-04-29 14:40:00 +0700

    change query to use NOW()

--- admin/system/news-save.php
+++ admin/system/news-save.php
@@ -8,7 +8,7 @@
     $url = "../../news/".$_POST['judul'].".html";
     $url = str_replace($url_edit,"-",$url);
     $gambar = $_POST['thumbnail'];
-    $query = mysqli_query($connection, "insert into berita values (NULL,'$judul','2020-04-28','$url','$gambar','$kategori')");
+    $query = mysqli_query($connection, "insert into berita values (NULL,'$judul',NOW(),'$url','$gambar','$kategori')");
     if($query){
         if($file=fopen($url, "w")){
             if(fwrite($file,$berita))

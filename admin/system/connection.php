d9cb4e3af3e3b0fede0c900646ff49f8ac3609a1
kanam129 <anamskansamal@gmail.com>
2020-04-30 09:46:48 +0700

    fix tabel di dashboard menambahkan text-overflow:ellipsis

--- admin/system/connection.php
+++ admin/system/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $connection = mysqli_connect("localhost", "root", "", "pindad-db");
+    $connection = mysqli_connect("127.0.0.1", "root", "", "pindad-db");
     if(mysqli_connect_errno()){
         echo mysqli_connect_errno();
     }
